780f52f4009682250aff4b57efd461a7bf9d670e
AFCMS <afcm.contact@gmail.com>
2021-03-06 12:00:19 +0100

    fixes

--- init.lua
+++ init.lua
@@ -1,8 +1,8 @@
-local has_itemdrop = minetest.get_modpath("itemdrop")
+local has_itemdrop = minetest.get_modpath("item_drop")
 
 minetest.register_on_mods_loaded(function()
 	for name,value in pairs(minetest.registered_nodes) do
-		if minetest.get_item_group(name, "plant") >= 1 then --FIXME
+		if minetest.get_item_group(name, "plant") >= 1 and not value.on_flood then
 			minetest.override_item(name, {
 				floodable = true,
 				on_flood = function(pos, oldnode, newnode)
