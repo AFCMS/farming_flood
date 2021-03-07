71ee276308272d23f7c971593fd5ae678b30b642
AFCMS <afcm.contact@gmail.com>
2021-03-07 16:55:22 +0100

    fix wrong item count

--- init.lua
+++ init.lua
@@ -1,5 +1,3 @@
-local has_itemdrop = minetest.get_modpath("item_drop")
-
 minetest.register_on_mods_loaded(function()
 	for name,value in pairs(minetest.registered_nodes) do
 		if minetest.get_item_group(name, "plant") >= 1
@@ -10,12 +8,6 @@ minetest.register_on_mods_loaded(function()
 				floodable = true,
 				on_flood = function(pos, oldnode, newnode)
 					minetest.dig_node(pos)
-					if not has_itemdrop then
-						local drops = minetest.get_node_drops(oldnode.name, nil)
-						for i = 1, #drops do
-							minetest.add_item(pos, drops[i])
-						end
-					end
 				end,
 			})
 		end
