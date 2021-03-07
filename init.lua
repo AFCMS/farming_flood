210940b5f6143d7c68b40a3bc441240e42df249c
AFCMS <afcm.contact@gmail.com>
2021-03-07 14:06:09 +0100

    make grass and flowers floodable

--- init.lua
+++ init.lua
@@ -2,7 +2,10 @@ local has_itemdrop = minetest.get_modpath("item_drop")
 
 minetest.register_on_mods_loaded(function()
 	for name,value in pairs(minetest.registered_nodes) do
-		if minetest.get_item_group(name, "plant") >= 1 and not value.on_flood then
+		if minetest.get_item_group(name, "plant") >= 1
+			or minetest.get_item_group(name, "grass") >= 1
+			or minetest.get_item_group(name, "dry_grass") >= 1
+			or minetest.get_item_group(name, "flower") >= 1 and not value.on_flood then
 			minetest.override_item(name, {
 				floodable = true,
 				on_flood = function(pos, oldnode, newnode)
